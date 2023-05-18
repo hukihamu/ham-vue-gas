f7e18933f11de172dfe8e20fa84a876cb70a0fb5
hukihamu <8084194+hukihamu @users.noreply.github.com>
2023-05-18 16:40:29 +0900

    readme

--- src/vue/initVue.ts
+++ src/vue/initVue.ts
@@ -1,5 +1,5 @@
 import {consoleLog} from '@/common/consoleLog'
-import { createApp, App } from 'vue'
+import { createApp, App , defineComponent} from 'vue'
 import { RouteRecordRaw, createRouter, createWebHistory, Router } from 'vue-router'
 
 export let router: Router
@@ -14,15 +14,23 @@ export function initVue(routes: RouteRecordRaw[], useApp: (app: App<Element>) =>
     }
 
 
-    useApp(createApp({}).use(router)).mount(mountContainer)
+    useApp(createApp(app)).mount(mountContainer)// .use(router)
 
-    router.afterEach(route => {
-        window.google.script.history.replace(undefined, route.query, route.path)
-    })
-    window.google.script.url.getLocation(location => {
-        const path = location.hash ? location.hash : '/'
-        const query = location.parameter
-        router.replace({ path, query })
-    })
+    // router.afterEach(route => {
+    //     window.google.script.history.replace(undefined, route.query, route.path)
+    // })
+    // window.google.script.url.getLocation(location => {
+    //     const path = location.hash ? location.hash : '/'
+    //     const query = location.parameter
+    //     router.replace({ path, query })
+    // })
 
 }
+
+const app = defineComponent({
+    name: 'App',
+    setup() {
+        console.log('app')
+    },
+    template: '<h2>app</h2>'
+})
