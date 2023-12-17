f832a8f8ad952fb299a581d067b993da25f862b7
hukihamu <8084194+hukihamu @users.noreply.github.com>
2023-12-17 15:55:33 +0900

    readme

--- bundle-dts.js
+++ bundle-dts.js
@@ -1,33 +1,19 @@
 const fs = require("fs");
 
 
+
 if (fs.existsSync('./dist/es/dts')){
   fs.rmdirSync('./dist/es/dts', {recursive: true})
 }
 
 const output = [];
-const processDir = (dir = "./dist/") => {
-  const files = fs.readdirSync(dir);
-  for (const index in files){
-    const item = files[index];
-    if (fs.lstatSync(`${dir}${item}`).isDirectory()){
-      processDir(`${dir}${item}/`)
-    }else if (item.match(/\.d\.ts$/)){
-      if (`${dir}`==="./dist/dts/@types/")continue;
-      if (`${dir}${item}`==="./dist/bundle.d.ts")continue;
-      output.push(fs.readFileSync(`${dir}/${item}`, 'utf8'))
-    }
-  }
+const dir =  "./src/@types/"
+const files = fs.readdirSync(dir);
+for (const item of files){
+  output.push(fs.readFileSync(`${dir}/${item}`, 'utf8'))
 }
 
-processDir();
-processDir("./src/@types/");
-
 if (output.length) {
-  fs.writeFileSync("./dist/bundle.d.ts",output.join("\n")
-      .replace(/export (default|\{).+;/g,"")
-      .replace(/import .+? '@.+?;/g,"")
-      .replace(/\/\/# sourceMappingURL=.+\.d\.ts\.map/g,"")
-    // + "\nexport default VueGas;"
-  );
+  output.push(fs.readFileSync("./dist/bundle.d.ts"))
+  fs.writeFileSync("./dist/bundle.d.ts",output.join("\n"));
 }
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
+import {dts} from 'rollup-plugin-dts'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,6 +18,10 @@ export default {
       file: 'dist/bundle.esm.js',
       format: 'esm'
     },
+    {
+      file: 'dist/bundle.d.ts',
+      format: 'es'
+    },
   ],
   plugins: [
     typescript(),
@@ -24,5 +29,6 @@ export default {
       babelHelpers: 'bundled',
       configFile: path.resolve(__dirname, ".babelrc.js"),
     }),
+    dts(),
   ]
 }
